de8f65d98c7ebbd3cb19024f7c36bf383df46b4f
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-06 19:16:38 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      return `I am ${i} strange loops.`;
+    } else {
+      `I am ${i} strange loops.`;
+    }
+  }
+}
+
